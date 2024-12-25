e057e5c8e412049db426a104791834aa97441954
Animehub4u <136146660+Animehub4u@users.noreply.github.com>
2024-12-25 16:43:13 +0530

    Add files via upload

---
+++ utils/banner.js
@@ -0,0 +1,12 @@
+const banner = `\n
+_________                        __           
+\_   ___ \_______ ___.__._______/  |_  ____   
+/    \  \/\_  __ <   |  |\____ \   __\/  _ \  
+\     \____|  | \/\___  ||  |_> >  | (  <_> ) 
+ \______  /|__|   / ____||   __/|__|  \____/  
+        \/        \/     |__|                 
+      
+                   HIVERA AUTO - BOT                
+     📢  Telegram Channel: https://t.me/CryptoNewbie110 `
+
+export default banner;
